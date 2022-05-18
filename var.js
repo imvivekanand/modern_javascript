92d35ac03a4afa147d25fdc93076feb4ce725964
Vivek Anand <104161816+ivivekanand@users.noreply.github.com>
2022-05-18 12:01:10 +0530

    Variable

---
+++ var.js
@@ -0,0 +1,17 @@
+//Var means variable. Before let and const, var is the only variable. It is not used now.
+var eggs = 12;
+eggs++
+
+//var is like let
+// not recommended
+
+// Variable Quiz:
+// 1) let eggCount = 42;
+// eggCount + 2;
+
+// 2) const rating = 7.5;
+// rating = 8;
+
+// 3)let wind_speed = 76;
+// wind_speed += 5;
+// wind_speed--;
