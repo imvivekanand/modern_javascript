4fa43548d2cdab02ca77bed18b3cbf9b8299c482
Vivek Anand <104161816+ivivekanand@users.noreply.github.com>
2022-05-18 11:49:35 +0530

    const variable

---
+++ introducing_constant.js
@@ -0,0 +1,9 @@
+const mango = 51;
+// mango++ - We cannot do this, it'll throw an error.
+const pi = 3.14159;
+const daysInWeek = 7;
+const minHeightForRide = 60;
+
+//Const is another type of variable.
+//Use for always constant value.
+//We cannot use a variable with the same name even one named with let and another with const.
