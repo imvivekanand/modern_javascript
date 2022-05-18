eefb7a0a43ad18475794c119528cae8ce68415b8
Vivek Anand <104161816+ivivekanand@users.noreply.github.com>
2022-05-18 16:34:21 +0530

    Strings

---
+++ strings.js
@@ -0,0 +1,11 @@
+//In JavaScript, Strings are pieces of text, or strings of characters.
+//We wrap them in quotes ' ' or " ".
+//Be consistent with quotes.
+"Vivek"
+"Vivek@1996"
+'99'
+'He said,"Work!"'
+let firstName = "Vivek"
+let lastName = "Anand"
+let fullName= firstName + " " + lastName;
+console.log(firstName+lastName)
