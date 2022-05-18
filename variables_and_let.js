7f8d0d9eb75d7f72bf4b692b559da2d9ce5d7255
Vivek Anand <104161816+ivivekanand@users.noreply.github.com>
2022-05-18 11:24:38 +0530

    variables and let

---
+++ variables_and_let.js
@@ -0,0 +1,13 @@
+//Variables are like "labeled jars" for a value in JavaScript.
+//We can store a value and give it a name, so that we can: recall it, use it, or change it later.
+let someName = "value";
+//Examples:
+let age = 26;
+
+let lemons = 4;
+let orange = 6;
+lemons = lemons - 1;
+console.log(lemons + orange)
+//We cannot name a variable with keywords, objects, document, number at start.
+//Use meaningfull name, camelCase
+//Cannot make a new variable with the same name.
