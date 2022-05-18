6cf8dfe8e98d80dc5a7ca96f62059fd8594b8769
Vivek Anand <104161816+ivivekanand@users.noreply.github.com>
2022-05-18 11:06:50 +0530

    numbers commit

---
+++ numbers.js
@@ -0,0 +1,18 @@
+// -> //to comment - Ignore the code
+console.log(3 + 4); // Addition
+console.log(12 - 4.567); //Substraction
+console.log(4 * 1.2); //Multiplication
+console.log(4 / 2) //Division
+console.log(27 % 2) //Modulo -> Remainder operator
+console.log(2 ** 3) //Exponential
+console.log(3 - 9 * 2) //Precedence [P- Parenthesis E- Exponent M- Multiplcation D- Division A- Addition S- Substraction]
+console.log(0 / 0) //NaN - Not a number
+console.log(1 + NaN) //Nan
+console.log(1 / 0) //Infinity
+console.log(-1 / 0) //-Infinity
+console.log(-0) //Negative Zero
+
+// Evaluate:
+// 1.5 + 1.5 * 2
+// (10 % 6) ** 2
+// 200 + 0/0
