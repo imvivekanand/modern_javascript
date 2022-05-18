7721ea867473ff22dd505f51e195f051a0feb025
Vivek Anand <104161816+ivivekanand@users.noreply.github.com>
2022-05-18 12:14:12 +0530

    bool

---
+++ booleans.js
@@ -0,0 +1,8 @@
+//Booleans are simply true or false value.
+let isLoggedIn = true;
+let gameOver = false;
+const isWaterWet = true;
+let isHungry = true;
+
+//We can change any type of value like bool, string, number etc to a variable already defined.
+//TypeScript works on the top of JavaScript and adds in strict rules like varibles cannot change types once assigned.
