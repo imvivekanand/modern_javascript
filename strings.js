a80cb3bace9a37209af57713dbef9a408228f2b9
Vivek Anand <104161816+ivivekanand@users.noreply.github.com>
2022-05-18 16:46:50 +0530

    s1

--- strings.js
+++ strings.js
@@ -9,3 +9,14 @@ let firstName = "Vivek"
 let lastName = "Anand"
 let fullName= firstName + " " + lastName;
 console.log(firstName+lastName)
+
+//String Indices
+//CHICKEN
+//0123456
+//Means C-0, H-1, I-2, C-3, K-4, E-5, N-6
+
+console.log("HELLO WORLD!".length);
+
+console.log(fullName[0])
+console.log(fullName[9])
+console.log(lastName[lastName.length-1])
